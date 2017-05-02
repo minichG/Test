2e404659b9ecdf1e56bf37e37a4be0a9f07e3139
galinaminich <galinaminich@gmail.com>
2017-05-02 10:06:24 +0300

    Добавьте файлы проекта.

---
+++ GitHuB1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHuB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age;
+            Console.WriteLine("Hello GitHub");
+            //Console.WriteLine("Enter your age: ");
+            //age = int.Parse(Console.ReadLine());
+            //Console.Write("Your age is " + age);
+        }
+    }
+}
